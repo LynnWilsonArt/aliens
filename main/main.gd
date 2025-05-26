3920ab5fb788e960197f26f5930b075f0ba52f25
“[charleslynn1969@gmail.com]” <charleslynn1969@gmail.com>
2025-05-26 09:38:21 -0500

    Implemented level change
    completed level change code in main.gd

--- main/main.gd
+++ main/main.gd
@@ -3,10 +3,12 @@ extends Node2D
 # Main program entry 
 # initalize some vars
 var enemy = preload("res://scenes/enemy/enemy.tscn")
-var number_enemies = Global.number_enemies
+var number_enemies = Global.number_enemies 
 var current_level = 1
+const  MAIN_NODE = 1
 
 func _ready():
+	
 	# Connect signal
 	Global.enemy_died.connect(on_enemy_died)
 	spawn_enemies()
@@ -16,28 +18,33 @@ func spawn_enemies():
 	for x in range(number_enemies -1):
 		var e = enemy.instantiate()
 		add_child(e)
+		
+
 
 
 func on_enemy_died() -> void:
+	# The number of enemies will include the main node
+	# everything must take into account the extra node
+	var enemies = get_tree().get_nodes_in_group("enemies") 
+	print(enemies)
+	print(enemies.size())
 
-	number_enemies -= 1
-	print("enemy")
-	print(number_enemies)
+	
 
-	if number_enemies == 0:
+	if enemies.size() -2 == 0:
 		current_level +=1
 		level_up()
 		
 func level_up():
 	if current_level == 2:
-		print("level")
-		print(current_level)
-		number_enemies = 2
+		print("moved to level 2")
+		number_enemies = 2 + MAIN_NODE # enemies group will include the main node
 		spawn_enemies()
 	if current_level == 3:
-		print("level")
-		print(current_level)
-		number_enemies =3
+		print("moved to level 3")
+		number_enemies =3 + MAIN_NODE # enemies group will include the main node
 		spawn_enemies()
+	if current_level == 4:
+		print("END OF GAME")
 	
 		
